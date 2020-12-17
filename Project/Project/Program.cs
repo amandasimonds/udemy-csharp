9b44b320094744bf48db244e6664ca07f3c99f6c
Amanda Tree <amandasimonds9@gmail.com>
2020-12-17 14:08:22 -0700

    classes and objects

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -2,11 +2,26 @@
 
 namespace Project
 {
-    class Program
+    public class Person
     {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
+        public static Person Parse(string str) //takes a string and returns a person object
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var p = Person.Parse("Bean"); //we parse the string and now we have a person object
+            p.Introduce("M");
         }
     }
 }
